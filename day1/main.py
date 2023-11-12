9cd0919a87fbdcb61612b6729a8b507ab4e3023a
Derek Bonilla <85432669+derekbonilla@users.noreply.github.com>
2023-11-11 22:53:09 -1000

    Add files via upload

---
+++ day1/main.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your favorite animal?\n")
+print("Your band name could be " "The " + city + " " + pet + "s")
+
+
+
+
